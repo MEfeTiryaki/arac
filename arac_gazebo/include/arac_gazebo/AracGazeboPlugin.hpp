ca711a7f641af65c87c6d7f7a60047fdd6f5c142
efe <m.efetiryaki@gmail.com>
2018-01-27 15:09:40 +0100

    new stuff in plugin

--- arac_gazebo/include/arac_gazebo/AracGazeboPlugin.hpp
+++ arac_gazebo/include/arac_gazebo/AracGazeboPlugin.hpp
@@ -1,10 +1,8 @@
 /*
-  Author : M. Efe Tiryaki
-*/
-
+ Author : M. Efe Tiryaki
+ */
 
 #pragma once
-
 
 // c++
 #include <chrono>
@@ -34,6 +32,8 @@
 
 // sensor msgs
 #include <sensor_msgs/JointState.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/JointState.h>
 
 #include "arac_msgs/AracState.h"
 
@@ -41,106 +41,112 @@
 #include <urdf/model.h>
 
 namespace gazebo {
-  class AracGazeboPlugin: public ModelPlugin
-  {
-    public:
-      // Constructor.
-      AracGazeboPlugin();
+class AracGazeboPlugin : public ModelPlugin
+{
+ public:
+  // Constructor.
+  AracGazeboPlugin();
 
-      // Destructor.
-      virtual ~AracGazeboPlugin();
+  // Destructor.
+  virtual ~AracGazeboPlugin();
 
-      // Implements Gazebo virtual load function.
-      virtual void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
+  // Implements Gazebo virtual load function.
+  virtual void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
 
-      // Overrides Gazebo init function.
-      virtual void Init();
+  // Overrides Gazebo init function.
+  virtual void Init();
 
-      // Overrides Gazebo reset function.
-      virtual void Reset();
+  // Overrides Gazebo reset function.
+  virtual void Reset();
 
-      virtual void OnUpdate();
-    protected:
-      // Reads parameters from the parameter server.
-      void readParameters(sdf::ElementPtr sdf);
+  virtual void OnUpdate();
+ protected:
+  // Reads parameters from the parameter server.
+  void readParameters(sdf::ElementPtr sdf);
 
-      void initJointStructures();
+  void initJointStructures();
 
-      void initSubscribers();
-      // Inits the ROS publishers.
-      void initPublishers();
-      // Inits the ROS services.
-      void initServices();
+  void initSubscribers();
+  // Inits the ROS publishers.
+  void initPublishers();
+  // Inits the ROS services.
+  void initServices();
 
-      // Reads simulation state.
-      virtual void readSimulation();
+  // Reads simulation state.
+  virtual void readSimulation();
 
-      // Writes simulation state.
-      virtual void writeSimulation();
+  // Writes simulation state.
+  virtual void writeSimulation();
 
-      // Retrieves URDF robot description from ROS parameter server.
-      virtual std::string getUrdfRobotDescription(const std::string& paramName) const;
+  // Retrieves URDF robot description from ROS parameter server.
+  virtual std::string getUrdfRobotDescription(const std::string& paramName) const;
 
-      // Ros node
-      ros::NodeHandle* nodeHandle_ ;
-      // Ensures gazebo methods are called sequentially
-      std::recursive_mutex gazeboMutex_;
+  // Ros node
+  ros::NodeHandle* nodeHandle_;
+  // Ensures gazebo methods are called sequentially
+  std::recursive_mutex gazeboMutex_;
+
+  // Name of the robot.
+  std::string robotName_;
+  // ROS robot description parameter name.
+  std::string robotDescriptionParamName_;
+  // Robot base link.
+  std::string robotBaseLink_;
+  // ROS robot description URDF string.
+  std::string robotDescriptionUrdfString_;
+  // ROS robot description URDF model.
+  urdf::Model robotDescriptionUrdfModel_;
+
+  // Frame names.
+  std::string frameBase_;
+  std::string frameOdometry_;
+  std::string frameWorld_;
+  std::string frameWorldGravityAligned_;
+
+  // Gazebo time step.
+  double gazeboTimeStep_ = 0.0;
+  // Time step for publishing simulation state.
+  double publishingTimeStep_ = 0.0;
+  // Simulation time stamp taken at the start of the last updateCb() function call.
+  common::Time lastStartUpdateSimTime_;
+  // System time stamp taken at the start of the last updateCb() function call.
+  std::chrono::time_point<std::chrono::steady_clock> lastStartUpdateSysTime_;
+  // Current inter-update simulation time step.
+  double updateSimTimeStep_ = 0.0;
+
+  // Model.
+  physics::ModelPtr model_;
+  // World update event.
+  event::ConnectionPtr updateConnection_;
+
+  // Robot base link pose (transforms vectors from base to world).
+  math::Pose robotBaseLinkPose_;
+  // Robot base link linear velocity in world frame.
+  math::Vector3 robotBaseLinkLinearVelocity_;
+  // Robot base link angular velocity in base frame.
+  math::Vector3 robotBaseLinkAngularVelocity_;
+
+  // Publisher
+  ros::Publisher robotStatePublisher_ ;
+  ros::Publisher imuDataPublisher_ ;
+  ros::Publisher actuatorDataPublisher_;
+
+  // Estimator Bool
+  bool isEstimatorUsed ;
+  // Actuators
+  std::vector<std::string> jointNames_;
+  std::unordered_map<std::string, int> jointNametoJointId_;
+  std::vector<gazebo::physics::JointPtr> jointPtrs_;
+  std::vector<int> jointTypes_;
+  std::vector<double> jointPositionsReset_;
+  std::vector<double> jointPositionLimitsLow_;
+  std::vector<double> jointPositionLimitsHigh_;
+  std::vector<double> jointTorqueLimits_;
+  std::vector<double> jointVelocityLimits_;
+  std::vector<double> jointPositionsDefault_;
 
 
-      // Name of the robot.
-      std::string robotName_;
-      // ROS robot description parameter name.
-      std::string robotDescriptionParamName_;
-      // Robot base link.
-      std::string robotBaseLink_;
-      // ROS robot description URDF string.
-      std::string robotDescriptionUrdfString_;
-      // ROS robot description URDF model.
-      urdf::Model robotDescriptionUrdfModel_;
 
-      // Frame names.
-      std::string frameBase_;
-      std::string frameOdometry_;
-      std::string frameWorld_;
-      std::string frameWorldGravityAligned_;
-
-      // Gazebo time step.
-      double gazeboTimeStep_ = 0.0;
-      // Time step for publishing simulation state.
-      double publishingTimeStep_ = 0.0;
-      // Simulation time stamp taken at the start of the last updateCb() function call.
-      common::Time lastStartUpdateSimTime_;
-      // System time stamp taken at the start of the last updateCb() function call.
-      std::chrono::time_point<std::chrono::steady_clock> lastStartUpdateSysTime_;
-      // Current inter-update simulation time step.
-      double updateSimTimeStep_ = 0.0;
-
-      // Model.
-      physics::ModelPtr model_;
-      // World update event.
-      event::ConnectionPtr updateConnection_;
-
-      // Robot base link pose (transforms vectors from base to world).
-      math::Pose robotBaseLinkPose_;
-      // Robot base link linear velocity in world frame.
-      math::Vector3 robotBaseLinkLinearVelocity_;
-      // Robot base link angular velocity in base frame.
-      math::Vector3 robotBaseLinkAngularVelocity_;
-
-      // Publisher
-      ros::Publisher robotStatePublisher_;
-
-      // Actuators
-      std::vector<std::string> jointNames_;
-      std::unordered_map<std::string,int> jointNametoJointId_;
-      std::vector<gazebo::physics::JointPtr> jointPtrs_;
-      std::vector<int> jointTypes_;
-      std::vector<double> jointPositionsReset_;
-      std::vector<double> jointPositionLimitsLow_;
-      std::vector<double> jointPositionLimitsHigh_;
-      std::vector<double> jointTorqueLimits_;
-      std::vector<double> jointVelocityLimits_ ;
-      std::vector<double> jointPositionsDefault_;
-  };
+};
 
 }
--- arac_gazebo/src/AracGazeboPlugin.cpp
+++ arac_gazebo/src/AracGazeboPlugin.cpp
@@ -1,6 +1,6 @@
 /*
-  Author : M. Efe Tiryaki
-*/
+ Author : M. Efe Tiryaki
+ */
 
 // param io
 #include <param_io/get_param.hpp>
@@ -8,157 +8,176 @@
 // arac gazebo
 #include "arac_gazebo/AracGazeboPlugin.hpp"
 
-
 namespace gazebo {
 
-  // Note : param_io is needed to use the getParam
-  using namespace param_io;
-  AracGazeboPlugin::AracGazeboPlugin(){}
+// Note : param_io is needed to use the getParam
+using namespace param_io;
+AracGazeboPlugin::AracGazeboPlugin()
+    : nodeHandle_()
+{
+}
 
-  AracGazeboPlugin::~AracGazeboPlugin(){}
+AracGazeboPlugin::~AracGazeboPlugin()
+{
+}
 
-  void AracGazeboPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
-  {
-    // To ensure that gazebo is not distrubed while loading
-    std::unique_lock<std::recursive_mutex> lock(gazeboMutex_);
-    nodeHandle_ = new ros::NodeHandle("~");
+void AracGazeboPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
+{
+  // To ensure that gazebo is not distrubed while loading
+  std::unique_lock<std::recursive_mutex> lock(gazeboMutex_);
+  nodeHandle_ = new ros::NodeHandle("~");
 
-    // Note : check if this is placed correctly
-    readParameters(sdf);
+  // Note : check if this is placed correctly
+  readParameters(sdf);
 
-    // Model
-    model_ = model ;
+  // Model
+  model_ = model;
 
-    // request the robot_description parameter
-    robotDescriptionUrdfString_ = getUrdfRobotDescription(robotDescriptionParamName_);
+  // request the robot_description parameter
+  robotDescriptionUrdfString_ = getUrdfRobotDescription(robotDescriptionParamName_);
 
-    // parse the URDF string into a URDF model structure
-    robotDescriptionUrdfModel_.initString(robotDescriptionUrdfString_);
+  // parse the URDF string into a URDF model structure
+  robotDescriptionUrdfModel_.initString(robotDescriptionUrdfString_);
 
-    // initialize ROS pub/sub/services
-    initPublishers();
-    initSubscribers();
-    initServices();
+  // initialize ROS pub/sub/services
+  initPublishers();
+  initSubscribers();
+  initServices();
 
-    // reset simulation variables
-    Reset();
+  // reset simulation variables
+  Reset();
 
-    // connect to world updates from Gazebo
-    updateConnection_ = event::Events::ConnectWorldUpdateBegin(
-          boost::bind(&AracGazeboPlugin::OnUpdate, this));
-  }
+  // connect to world updates from Gazebo
+  updateConnection_ = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&AracGazeboPlugin::OnUpdate, this));
+}
 
-  void AracGazeboPlugin::readParameters( sdf::ElementPtr sdf )
-  {
-    robotName_  = sdf->GetElement("robotName")->Get<std::string>();
-    robotBaseLink_ = sdf->GetElement("robotBaseLink")->Get<std::string>();
-    robotDescriptionParamName_ = sdf->GetElement("robotDescription")->Get<std::string>();
-    const double statePublisherRate = sdf->GetElement("statePublisherRate")->Get<double>();
-    publishingTimeStep_ = (statePublisherRate > 0.0) ? 1.0 / statePublisherRate : 0.0;
+void AracGazeboPlugin::readParameters(sdf::ElementPtr sdf)
+{
+  robotName_ = sdf->GetElement("robotName")->Get<std::string>();
+  robotBaseLink_ = sdf->GetElement("robotBaseLink")->Get<std::string>();
+  robotDescriptionParamName_ = sdf->GetElement("robotDescription")->Get<std::string>();
+  const double statePublisherRate = sdf->GetElement("statePublisherRate")->Get<double>();
+  publishingTimeStep_ = (statePublisherRate > 0.0) ? 1.0 / statePublisherRate : 0.0;
 
-    getParam(*nodeHandle_, "frame/base/name", frameBase_);
-    getParam(*nodeHandle_, "frame/odometry/name", frameOdometry_);
-    getParam(*nodeHandle_, "frame/world/name", frameWorld_);
-    getParam(*nodeHandle_, "frame/world_gravity_aligned/name", frameWorldGravityAligned_);
+  getParam(*nodeHandle_, "frame/base/name", frameBase_);
+  getParam(*nodeHandle_, "frame/odometry/name", frameOdometry_);
+  getParam(*nodeHandle_, "frame/world/name", frameWorld_);
+  getParam(*nodeHandle_, "frame/world_gravity_aligned/name", frameWorldGravityAligned_);
 
-    getParam(*nodeHandle_, "joint_states/default_positions", jointPositionsDefault_);
+  getParam(*nodeHandle_, "joint_states/default_positions", jointPositionsDefault_);
 
-  }
+  isEstimatorUsed = False;
+}
 
+// Note : RSL code for getting model of the robot
+std::string AracGazeboPlugin::getUrdfRobotDescription(const std::string& paramName) const
+{
+  std::cout << "[" << robotName_
+            << "GazeboPlugin::getUrdfRobotDescription] Loading urdf description." << std::endl;
 
-  // Note : RSL code for getting model of the robot
-  std::string AracGazeboPlugin::getUrdfRobotDescription(const std::string& paramName) const {
-    std::cout <<"[" << robotName_ << "GazeboPlugin::getUrdfRobotDescription] Loading urdf description."<<std::endl;
-
-    std::string urdfString;
-    const double timeOut = 5.0; // [s]
-    auto start = std::chrono::steady_clock::now();
-    while (urdfString.empty()) {
-      std::string searchParamName;
-      if (nodeHandle_->searchParam(paramName, searchParamName)) {
-        std::cout <<"[" << robotName_ << "GazeboPlugin::getUrdfRobotDescription] Waiting for model URDF in parameter " << searchParamName << " on the ROS parameter server."<<std::endl;
-        nodeHandle_->getParam(searchParamName, urdfString);
-      }
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
-      auto now = std::chrono::steady_clock::now();
-
-      if ((std::chrono::duration_cast<std::chrono::seconds>(now - start)).count() >= timeOut) {
-        std::cout <<"[" << robotName_ << "GazeboPlugin::getUrdfRobotDescription] Timeout while loading urdf!"<<std::endl;
-
-        break;
-      }
+  std::string urdfString;
+  const double timeOut = 5.0;  // [s]
+  auto start = std::chrono::steady_clock::now();
+  while (urdfString.empty()) {
+    std::string searchParamName;
+    if (nodeHandle_->searchParam(paramName, searchParamName)) {
+      std::cout << "[" << robotName_
+                << "GazeboPlugin::getUrdfRobotDescription] Waiting for model URDF in parameter "
+                << searchParamName << " on the ROS parameter server." << std::endl;
+      nodeHandle_->getParam(searchParamName, urdfString);
     }
-    std::cout <<"[" << robotName_ << "GazeboPlugin::getUrdfRobotDescription] Received urdf from parameter server."<<std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    auto now = std::chrono::steady_clock::now();
 
-    return urdfString;
-  }
+    if ((std::chrono::duration_cast<std::chrono::seconds>(now - start)).count() >= timeOut) {
+      std::cout << "[" << robotName_
+                << "GazeboPlugin::getUrdfRobotDescription] Timeout while loading urdf!"
+                << std::endl;
 
-  void AracGazeboPlugin::initJointStructures(){
-    // Todo : Make this more generic
-    jointNames_.push_back("LF_WH");
-    jointNames_.push_back("RF_WH");
-    jointNames_.push_back("LH_WH");
-    jointNames_.push_back("RH_WH");
-
-    int i = 0 ;
-    for (const auto jointName: jointNames_){
-      jointNametoJointId_.insert(std::make_pair(jointName,i++));
-    }
-
-    // Init the joint structures.
-    for (const auto jointName : jointNames_ ) {
-
-      auto joint = robotDescriptionUrdfModel_.getJoint(jointName);
-      if (!joint) {
-        std::cout<<"gazebo_ros_control : "<< "Joint named '" << jointName << "' does not exist in the URDF model."<<std::endl;
-        return;
-      }
-
-      auto jointPtr =  model_->GetJoint(jointName);
-
-      if (!jointPtr) {
-        std::cout<<"gazebo_ros_control : "<<  "Joint named '" << jointName << "' does not exist in Gazebo."<<std::endl;
-        return;
-      }
-
-      jointPtrs_.push_back(jointPtr);
-
-      // Set joint position to default initial position
-      jointPtrs_.back()->SetPosition(0, jointPositionsDefault_[jointNametoJointId_[jointName]]);
-
-      jointTypes_.push_back(joint->type) ;
-      jointPositionsReset_.push_back( jointPositionsDefault_[jointNametoJointId_[jointName]] );
-      jointPositionLimitsLow_.push_back(joint->limits->lower);
-      jointPositionLimitsHigh_.push_back( joint->limits->upper);
-      jointTorqueLimits_.push_back( joint->limits->effort);
-      jointVelocityLimits_.push_back( joint->limits->velocity);
+      break;
     }
   }
+  std::cout << "[" << robotName_
+            << "GazeboPlugin::getUrdfRobotDescription] Received urdf from parameter server."
+            << std::endl;
 
-  void AracGazeboPlugin::initPublishers()  {
-    // Actuator readings.
-    std::cout << "Initializing Publishers"<< std::endl;
+  return urdfString;
+}
 
-    // Joint State Publishers
+void AracGazeboPlugin::initJointStructures()
+{
+  // Todo : Make this more generic
+  jointNames_.push_back("LF_WH");
+  jointNames_.push_back("RF_WH");
+  jointNames_.push_back("LH_WH");
+  jointNames_.push_back("RH_WH");
 
-
-    // Base State Publisher
-
-
-    // Actuator Readings Publisher
-
+  int i = 0;
+  for (const auto jointName : jointNames_) {
+    jointNametoJointId_.insert(std::make_pair(jointName, i++));
   }
 
-  void AracGazeboPlugin::initSubscribers()  {
-    // Actuator readings.
-    std::cout << "Initializing Publishers"<< std::endl;
+  // Init the joint structures.
+  for (const auto jointName : jointNames_) {
 
-    // Actuator Command Subscriber
+    auto joint = robotDescriptionUrdfModel_.getJoint(jointName);
+    if (!joint) {
+      std::cout << "gazebo_ros_control : " << "Joint named '" << jointName
+                << "' does not exist in the URDF model." << std::endl;
+      return;
+    }
 
-    
+    auto jointPtr = model_->GetJoint(jointName);
 
+    if (!jointPtr) {
+      std::cout << "gazebo_ros_control : " << "Joint named '" << jointName
+                << "' does not exist in Gazebo." << std::endl;
+      return;
+    }
+
+    jointPtrs_.push_back(jointPtr);
+
+    // Set joint position to default initial position
+    jointPtrs_.back()->SetPosition(0, jointPositionsDefault_[jointNametoJointId_[jointName]]);
+
+    jointTypes_.push_back(joint->type);
+    jointPositionsReset_.push_back(jointPositionsDefault_[jointNametoJointId_[jointName]]);
+    jointPositionLimitsLow_.push_back(joint->limits->lower);
+    jointPositionLimitsHigh_.push_back(joint->limits->upper);
+    jointTorqueLimits_.push_back(joint->limits->effort);
+    jointVelocityLimits_.push_back(joint->limits->velocity);
   }
+}
 
+void AracGazeboPlugin::initPublishers()
+{
+  // Actuator readings.
+  std::cout << "Initializing Publishers" << std::endl;
 
-  GZ_REGISTER_MODEL_PLUGIN(AracGazeboPlugin)
+  // Robot State Publishers
+  robotStatePublisher_ = nodeHandle_->advertise<arac_msgs::AracState>(robotName_ + "JointState", 1);
+
+  if (isEstimatorUsed) {
+    // Todo : implement Imu data for state estimation
+    imuDataPublisher_ = nodeHandle_->advertise<sensor_msgs::Imu>(robotName_ + "ImuData", 1);
+    // Todo : implement joint data for state estimation
+    actuatorDataPublisher_ = nodeHandle_->advertise<sensor_msgs::JointState>(
+        robotName_ + "_ActuatorData", 1);
+  }
+}
+
+void AracGazeboPlugin::initSubscribers()
+{
+  // Actuator readings.
+  std::cout << "Initializing Publishers" << std::endl;
+
+  // Actuator Command Subscriber
+  nh.subscribe(robotName_+"ActuatorCommand",
+                      1000,
+                      &MBSteeringPlugin::setReferenceVelocities,
+                      this);
+}
+
+GZ_REGISTER_MODEL_PLUGIN(AracGazeboPlugin)
 }
