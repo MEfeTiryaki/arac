0e46659863a2b4e31e7a219efc882b74365a43e9
efe <m.efetiryaki@gmail.com>
2018-02-13 18:39:18 +0100

    gazebo benzetimi için devitec taslamı oluşturuldu.

---
+++ arac_model/src/Devitec/Denetleyici.cpp
@@ -0,0 +1,38 @@
+/*/*
+ name : Denetleyici.cpp
+ Author : Selçuk Ercan , M. Efe Tiryaki
+
+*/
+
+
+#include "arac_model/Devitec/Denetleyici.hpp"
+
+namespace kuco {
+
+// Note : param_io is needed to use the getParam
+Denetleyici::Denetleyici()
+{
+}
+
+Denetleyici::~Denetleyici()
+{
+}
+
+void Denetleyici::initilize()
+{
+}
+
+void Denetleyici::create()
+{
+}
+
+void Denetleyici::advance()
+{
+}
+
+
+void Denetleyici::reset()
+{
+}
+
+} /* namespace kuco*/
---
+++ arac_model/src/Devitec/DevitecModelBase.cpp
@@ -0,0 +1,38 @@
+/*/*
+ name : DevitecModelBase.cpp
+ Author : Selçuk Ercan , M. Efe Tiryaki
+
+*/
+
+
+#include "arac_model/Devitec/DevitecModelBase.hpp"
+
+namespace kuco {
+
+// Note : param_io is needed to use the getParam
+DevitecModelBase::DevitecModelBase()
+{
+}
+
+DevitecModelBase::~DevitecModelBase()
+{
+}
+
+void DevitecModelBase::initilize()
+{
+}
+
+void DevitecModelBase::create()
+{
+}
+
+void DevitecModelBase::advance()
+{
+}
+
+
+void DevitecModelBase::reset()
+{
+}
+
+} /* namespace kuco*/
