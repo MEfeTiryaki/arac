1ddca12188c3ceb3aefa60b6f8b0a69335e4bd74
sadetra <m.efetiryaki@gmail.com>
2018-04-13 10:37:31 +0300

    Durum kestirimci eklendi ve yeni bir denetleyici altyapisi olusturuldu

---
+++ arac_controller/include/arac_controller/AracOLController.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "arac_model/Model/AracModel.hpp"
+#include "arac_controller/LinearOpenLoopController.hpp"
+#include <vector>
+
+namespace kuco {
+
+using Model = kuco::AracModel;
+class AracOLController : public LinearOpenLoopController<Model>
+{
+ public:
+  // Constructor.
+//  aracController(kuco::State& state);
+  AracOLController(Model& model);
+
+  // Destructor.
+  virtual ~AracOLController();
+
+  // Init
+  virtual void initilize() override;
+
+  // Parameters init
+  virtual void readParameters() override;
+
+ protected:
+
+  virtual void getState() override;
+
+  virtual void setActuatorCommand() override;
+
+  // tekerlek yaricapi
+  double rWheel_ ;
+  // tekerlek uzakligi
+  double lWheel_ ;
+};
+
+} /* kuco adligi*/
---
+++ arac_controller/src/AracOLController.cpp
@@ -0,0 +1,74 @@
+#include <arac_controller/AracOLController.hpp>
+#include <param_io/get_param.hpp>
+
+namespace kuco {
+
+// Note : param_io is needed to use the getParam
+using namespace param_io;
+
+// Todo : make controllerInput length adjustable
+AracOLController::AracOLController(Model& model)
+    : LinearOpenLoopController<Model>(model),
+      rWheel_(0),
+      lWheel_(0)
+{
+}
+
+AracOLController::~AracOLController()
+{
+}
+
+void AracOLController::initilize()
+{
+  // Durum uzayi eleman sayisi
+  n_ = 2;
+  // Girdi sayisi
+  l_ = 2;
+
+  //
+  rWheel_ = 0.15;
+  lWheel_ = 0.2;
+
+  //
+  x_.setZero(n_);
+  u_.setZero(l_);
+
+  // Todo : arac icin B yi tanimla
+  B_.setZero(l_, n_);
+  B_.row(0) << -rWheel_ / 2, -rWheel_ / 2;
+  B_.row(1) << rWheel_ / 2 / lWheel_, -rWheel_ / 2 / lWheel_;
+}
+
+void AracOLController::readParameters()
+{
+
+}
+
+void AracOLController::getState()
+{
+  // Todo : Burayi Kulman konacligina cevirmek gerekiyor
+  x_[0] = model_.getGovde().getDesiredState().getVelocityInWorldFrame()[0];
+  x_[1] = model_.getGovde().getDesiredState().getAngularVelocityInWorldFrame()[2];
+}
+
+void AracOLController::setActuatorCommand()
+{
+  std::vector<Tekerlek*>& wheels = model_.getTekerlek();
+  kuco::AngularVelocity input;
+
+  // LF
+  input << 0.0, 0.0, u_[0];
+  wheels[0]->getDesiredState().setAngularVelocityInWorldFrame(input);
+  // RF
+  input << 0.0, 0.0, u_[0];
+  wheels[1]->getDesiredState().setAngularVelocityInWorldFrame(input);
+  // LH
+  input << 0.0, 0.0, u_[1];
+  wheels[2]->getDesiredState().setAngularVelocityInWorldFrame(input);
+  // RH
+  input << 0.0, 0.0, u_[1];
+  wheels[3]->getDesiredState().setAngularVelocityInWorldFrame(input);
+
+}
+
+} /* namespace kuco*/
